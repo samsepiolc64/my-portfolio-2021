2a95e756ee93032a6d92e20e975de41173928b29
Michal Jarmuzek <mjarmuzek@wp.pl>
2021-08-10 19:39:52 +0200

    first comm

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function toggleMenu(){
+    const menuToggle = document.querySelector('.toggle');
+    menuToggle.classList.toggle('active'); //dodaje dodatkowa klase active do przycisku, po kliknieciu w niego
+}
