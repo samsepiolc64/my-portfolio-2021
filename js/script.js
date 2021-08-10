9f87ffe9e3a2279736f7b9a245ab2026f526dd2e
Michal Jarmuzek <mjarmuzek@wp.pl>
2021-08-10 22:13:47 +0200

    add menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 function toggleMenu(){
     const menuToggle = document.querySelector('.toggle');
+    const sidebar = document.querySelector('.sidebar');
     menuToggle.classList.toggle('active'); //dodaje dodatkowa klase active do przycisku, po kliknieciu w niego
+    sidebar.classList.toggle('active');
 }
